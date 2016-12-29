ae2a3bcdffb857b86a6ee501000f55f6bb428a73
Jon Borgonia <jon@gomagames.com>
2016-12-29 10:52:42 -1000

    load spritesheet and create player sprite

--- js/game.js
+++ js/game.js
@@ -2,13 +2,15 @@
   const GAME_WIDTH = 460;
   const GAME_HEIGHT = 600;
   const GAME_CONTAINER_ID = 'game';
+  const GFX = 'gfx';
+  let player;
 
   const preload = _ => {
-
+    game.load.spritesheet(GFX, 'assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
   };
 
   const create = _ => {
-
+    player = game.add.sprite(100, 100, GFX, 8);
   };
 
   const update = _ => {
